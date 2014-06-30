352959ddb25b5090200cf1a8904fcec96b02f3e0
Channely <qijie29896@gmail.com>
2014-06-30 14:31:42 +0800

    write the console.js code to prepare to change to a new console style

--- codes/javascript_template/js/console.js
+++ codes/javascript_template/js/console.js
@@ -26,7 +26,12 @@ console.log = function (arg) {
     }
     if(count>=0){
         logs[count] = arg;
-        document.write("<h4 style='padding:15px;color: " + color + "'>" + arg + "</h4>");
+        write_the_console_info_into_web_page(arg,color);
     }
     count++;
 };
+
+function write_the_console_info_into_web_page(arg,color){
+// next line is the key code to show message on the web console.
+    document.write("<h4 style='padding:15px;color: " + color + "'>" + arg + "</h4>");
+}
