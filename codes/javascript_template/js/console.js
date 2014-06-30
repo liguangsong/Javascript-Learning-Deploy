87770f4f4d947cf0b035dd59ae289aa9ea265e64
Channely <qijie29896@gmail.com>
2014-06-30 14:34:20 +0800

    rename function ofr better meaning

--- codes/javascript_template/js/console.js
+++ codes/javascript_template/js/console.js
@@ -26,12 +26,12 @@ console.log = function (arg) {
     }
     if(count>=0){
         logs[count] = arg;
-        write_the_console_info_into_web_page(arg,color);
+        console_log_on_web_page(arg,color);
     }
     count++;
 };
 
-function write_the_console_info_into_web_page(arg,color){
+function console_log_on_web_page(arg,color){
 // next line is the key code to show message on the web console.
     document.write("<h4 style='padding:15px;color: " + color + "'>" + arg + "</h4>");
 }
