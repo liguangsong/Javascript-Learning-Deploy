6a20185b3ee28f2f9178d5a330d5216141b87ff1
Channely <qijie29896@gmail.com>
2014-07-03 16:37:55 +0800

    debug

--- codes/javascript_template/challenge_3_1.js
+++ codes/javascript_template/challenge_3_1.js
@@ -1,5 +1,4 @@
 //在第二行代码中两个数字之间添加一个算数运算符,使计算结果的值大于 10000
 var result = 400  400;
-console.log(result);
-console.log(result++); //result先参与代码的执行,然后自己的值累加1.
-console.log(++result); //result先将自己的值累加一,再参与代码执行.
+console.log("计算结果是:" + result++);
+console.log("计算结果两次累加后的值:" + ++result);
