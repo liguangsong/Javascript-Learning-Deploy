ac035f9c2c05588a5900c752c5abecf1634adb26
Channely <qijie29896@gmail.com>
2014-07-07 17:17:35 +0800

    debug

--- codes/javascript_template/challenge_4_2.js
+++ codes/javascript_template/challenge_4_2.js
@@ -1,9 +1,9 @@
-//请给第六行中profile对象的grade属性添加一个合适的数字值.
+//通过分析第九行中的条件判断语句,给第六行中profile对象的grade属性添加一个合适的数字值.
 //使console输出: 嘿Tom!你通过了本学期的Maths考试.
 var profile = {
     name : "Tom",
     course : "Maths",
-    grade :
+    score :
 };
 
-console.log("嘿" + profile.name + "!你" + (profile.grade >= 60 ? "通过了" : "没通过") + "本学期的" + profile.course + "考试.");
+console.log("嘿" + profile.name + "!你" + (profile.score >= 60 ? "通过了" : "没通过") + "本学期的" + profile.course + "考试.");
