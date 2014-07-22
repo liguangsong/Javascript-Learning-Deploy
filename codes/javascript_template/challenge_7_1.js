0879efbb85a0da60974b6a246255c7cd0ec98170
Channely <qijie29896@gmail.com>
2014-07-22 15:11:17 +0800

    new code practicenew code practice

--- codes/javascript_template/challenge_7_1.js
+++ codes/javascript_template/challenge_7_1.js
@@ -1,1 +1,9 @@
-var demo;
+function get_type(data) {
+    //在下一行添加代码, 定义变量 type ,并将 参数 data 的类型值赋给 type.
+
+
+    var result = "该数据的类型是:" + type;
+    console.log(result);
+}
+var my_data = prompt("请输入想查看类型的数据!")
+get_type(my_data);
