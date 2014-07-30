227e200a5da10ac030b126b71dad7beaebc5536f
Channely <qijie29896@gmail.com>
2014-07-30 15:13:50 +0800

    test

--- codes/javascript_template/challenge_7_1.js
+++ codes/javascript_template/challenge_7_1.js
@@ -1,6 +1,6 @@
 var all_data = {a: 123, b: 'hello', c: null, d: ['hello', 'world', '!']};
 function get_type(data) {
-    //在下一行添加代码，定义变量 type ，并将 参数 data 的类型值赋给 type。
+    //在下一行添加代码，定义变量 type ，并将 参数 data 的类型值赋给 type 。
 
 
     var result = "该数据的类型是:" + type;
